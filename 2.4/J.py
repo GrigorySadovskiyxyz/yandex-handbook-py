ca3496773f740b57f8eeb689e03bc57a8ac9e38a
grigory <sadovskiy.grigory@yandex.ru>
2023-06-24 20:15:16 +0300

    2.4 and 3.2 sections changed

--- 2.4/J.py
+++ 2.4/J.py
@@ -1,18 +1,6 @@
-all_char = []
-d = {}
-while (n := input().upper()) != "ФИНИШ":
-    n = n.lower().replace(" ", "")
-    all_char = [*all_char, *n]
-letters = set()
-for i in all_char:
-    letters.add(i)
-for let in letters:
-    d[let] = all_char.count(let)
-sorted_values = str(max(sorted(d.values())))
-sorted_dict = {}
-for k in d.keys():
-    if d[k] == int(sorted_values):
-        sorted_dict[k] = d[k]
-sorted_dict = sorted([*sorted_dict.keys()])[0]
-print(sorted_dict)
-
+orange = int(input())
+res = [(i, j, orange - i - j)
+       for i in range(1, orange-1) 
+       for j in range(1, orange-i)]
+for i in res:
+    print(*i)
