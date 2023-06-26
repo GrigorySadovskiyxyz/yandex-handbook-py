9dab5fe3a4f47af99d78715d04a80431fb21e909
grigory <sadovskiy.grigory@yandex.ru>
2023-06-26 22:53:19 +0300

    modified 3.4

--- 3.4/K.py
+++ 3.4/K.py
@@ -2,6 +2,8 @@ from itertools import product
 
 n = int(input())
 m = int(input())
-counter = 1
-x = list(product([x for x in range(1, )]))
+counter = 0
+for i in range(1, n + 1):
+    print(*product([counter], range(1, m + 1)))
+    counter += m
 print(x)
