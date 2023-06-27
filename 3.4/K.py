8b6a1df4477b790d2b46c2e2ab97e2f68f3585bc
grigory <sadovskiy.grigory@yandex.ru>
2023-06-27 18:00:56 +0300

    K.py 3.4

--- 3.4/K.py
+++ 3.4/K.py
@@ -1,9 +1,8 @@
-from itertools import product
+from itertools import product, starmap
 
 n = int(input())
 m = int(input())
 counter = 0
 for i in range(1, n + 1):
-    print(*product([counter], range(1, m + 1)))
+    print(*starmap(lambda x, y: str(x + y).rjust(len(str(n * m))), product([counter], range(1, m + 1))))
     counter += m
-print(x)
