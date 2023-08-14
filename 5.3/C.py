b177067021c9a689ac05e3c6c38a0832d9496b53
grigory <sadovskiy.grigory@yandex.ru>
2023-08-14 23:48:37 +0300

    C.py 5.3

--- 5.3/C.py
+++ 5.3/C.py
@@ -1,6 +1,10 @@
-def only_positive_even_sum(num1, num2):
-    if type(num1) is not int or type(num2) is not int:
-        raise TypeError
-    if not (num1 > 0 and not num1 % 2) or not (num2 > 0 and not num2 % 2):
-        raise ValueError
-    return num1 + num2
+class Broken:
+    def __repr__(self):
+        raise Exception
+
+
+try:
+    a = Broken()
+    func(a)
+except Exception:
+    print('Ура! Ошибка!')
