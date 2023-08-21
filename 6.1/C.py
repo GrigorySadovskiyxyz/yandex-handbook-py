1443ea390ab57223b36f16035259c6667fe33d69
grigory <sadovskiy.grigory@yandex.ru>
2023-08-21 23:11:10 +0300

    D.py wrong

--- 6.1/C.py
+++ 6.1/C.py
@@ -1,10 +1,12 @@
-class Broken:
-    def __repr__(self):
-        raise Exception
+from itertools import combinations
 
+nm = input().split()
+total_variants = 0
+vasya_variants = 0
+N, M = nm[0], nm[1]
 
-try:
-    a = Broken()
-    func(a)
-except Exception:
-    print('Ура! Ошибка!')
+for combination in combinations(range(int(N)), int(M)):
+    total_variants += 1
+    if 0 in combination:
+        vasya_variants += 1
+print(int(vasya_variants), int(total_variants))
--- 6.1/D.py
+++ 6.1/D.py
@@ -1,6 +1,6 @@
-def only_positive_even_sum(num1, num2):
-    if type(num1) is not int or type(num2) is not int:
-        raise TypeError
-    if not (num1 > 0 and not num1 % 2) or not (num2 > 0 and not num2 % 2):
-        raise ValueError
-    return num1 + num2
+from math import prod
+
+nums = [float(x) for x in input().split()]
+
+n = len(nums)
+print(prod(nums) ** (1 / n))
